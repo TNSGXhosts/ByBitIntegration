fd6fce04d5937b398a617bb4d23d6315774a972e
Vit <79087978+TNSGXhosts@users.noreply.github.com>
2025-05-14 21:15:23 +0200

    refactor: inject IConfiguration into BybitClient and initialize SDK clients internally

--- ByBit/BybitClient.cs
+++ ByBit/BybitClient.cs
@@ -1,6 +1,8 @@
+
 using bybit.net.api.ApiServiceImp;
 using bybit.net.api.Models;
 using bybit.net.api.Models.Market;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace Axi.Bybit
@@ -10,20 +12,23 @@ namespace Axi.Bybit
         private readonly BybitMarketDataService _market;
         private readonly BybitTradeService _trade;
 
-        public BybitClient(string apiKey, string apiSecret, bool useTestnet)
+        public BybitClient(IConfiguration configuration)
         {
+            var apiKey = configuration["BYBIT_API_KEY"];
+            var apiSecret = configuration["BYBIT_API_SECRET"];
+            var useTestnet = configuration.GetValue<bool>("BYBIT_USE_TESNET");
+
             _trade = new BybitTradeService(apiKey, apiSecret, debugMode: useTestnet);
             _market = new BybitMarketDataService(apiKey, apiSecret, useTestnet);
         }
 
-        public async Task<List<KlineDto>> GetKlinesAsync(string symbol, MarketInterval interval, int limit)
+        public async Task<List<KlineDto>> GetKlines@³ync(string symbol, MarketInterval interval, int limit)
         {
             var response = await _market.GetMarketKline(Category.SPOT, symbol, interval, limit);
 
             if (response != null)
             {
                 var responseObject = JsonConvert.DeserializeObject<KlineListDto>(response);
-
                 return responseObject?.Klines ?? new List<KlineDto>();
             }
 
