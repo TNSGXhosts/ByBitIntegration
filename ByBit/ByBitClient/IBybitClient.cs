0d052f0d9213fe40b2487793581e344c39e75e32
Vit <79087978+TNSGXhosts@users.noreply.github.com>
2025-05-14 23:01:25 +0200

    feat: add PlaceOrderAsync method to IBybitClient interface

--- ByBit/ByBitClient/IBybitClient.cs
+++ ByBit/ByBitClient/IBybitClient.cs
@@ -1,9 +1,10 @@
 using bybit.net.api.Models.Market;
 using BybitModels;
 
-namespace Bybit.BybitClient;
+namespace Bybit.BybicClient;
 
 public interface IBybitClient
 {
     Task<List<Kline>> GetKlinesAsync(string symbol, MarketInterval interval, int limit);
+    Task<PlaceOrderResult> PlaceOrderAsync(string symbol, Side side, OrderType orderType, decimal qty);
 }
