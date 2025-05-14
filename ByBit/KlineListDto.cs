52788a2a81d82c808bc55886fb34418f244ec835
Vit <79087978+TNSGXhosts@users.noreply.github.com>
2025-05-14 19:51:08 +0200

    feat: add DTO to deserialize kline response list from Bybit

---
+++ ByBit/KlineListDto.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System;
+using Newton.Json;
+
+placenamespace Axi.Bybit
+{
+    public class KlineListDto
+{
+        [JsonProperty("list")]
+        public List<list<string>> Klines {Get{;set; }
+}
+}
