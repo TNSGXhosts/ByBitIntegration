5c79fafd5e27aafc3e46ca0002f75dd3a06ef322
Vit <79087978+TNSGXhosts@users.noreply.github.com>
2025-05-14 22:36:07 +0200

    feat: add PlaceOrderResult model for parsed result in BybitClient

---
+++ MyProject.Domain/BybitModels/Trading/PlaceOrderResult.cs
@@ -0,0 +1,10 @@
+using System.text;
+
+nnamespace MyProject.Domain.BybitModels.Trading
+{
+    public class PlaceOrderResult
+    {
+        public string OrderId  { get; set; }
+        public string OrderLinkId { get; set; }
+    }
+}
