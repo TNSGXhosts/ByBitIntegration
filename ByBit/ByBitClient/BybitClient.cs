a97c7a804ea5acd0817e092d6b4e9c48cea4adeb
Vit <79087978+TNSGXhosts@users.noreply.github.com>
2025-05-15 15:39:40 +0200

    feat: add method to retrieve wallet balance using BybitAccountService

--- ByBit/ByBitClient/BybitClient.cs
+++ ByBit/ByBitClient/BybitClient.cs
@@ -2,6 +2,7 @@ using bybit.net.api.ApiServiceImp;
 using bybit.net.api.Models;
 using bybit.net.api.Models.Market;
 using bybit.net.api.Models.Trade;
+using bybit.net.api.Models.Account;
 using BybitModels;
 using Microsoft.Extensions.Options;
 using MyProject.Domain.BybitModels.Prices;
@@ -14,20 +15,22 @@ namespace Bybit.BybitClient
     {
         private readonly BybitMarketDataService _market;
         private readonly BybitTradeService _trade;
+        private readonly BybitAccountService _account;
 
         public BybitClient(IOptions<BybitSettings> options)
         {
             var apiKey = options.Value.ApiKey;
             var apiSecret = options.Value.SecretKey;
             var useTestnet = options.Value.UseTestnet;
-            
+
             if (apiKey == null || apiSecret == null)
                 throw new ArgumentException("Bybit API key or secret is not configured");
             _trade = new BybitTradeService(apiKey, apiSecret, debugMode: useTestnet);
             _market = new BybitMarketDataService("https://api-testnet.bybit.com", debugMode: useTestnet);
+            _account = new BybitAccountService(apiKey, apiSecret, debugMode: useTestnet);
         }
 
-        public async Task<List<Kline>> GetKlinesAsync(string symbol, MarketInterval interval, int? limit = null)
+        public async Task<List<Kline>> GetKlinesAsync(optional string symbol, MarketInterval interval, int? limit = null)
         {
             var response = await _market.GetMarketKline(Category.INVERSE, symbol, interval, limit: limit);
             if (!string.IsNullOrEmpty(response))
@@ -41,14 +44,19 @@ namespace Bybit.BybitClient
 
         public async Task<PlaceOrderResult> PlaceOrderAsync(string symbol, Side side, OrderType orderType, decimal qty)
         {
-            var response = await _trade.PlaceOrder(Category.SPOT,symbol, side, orderType, qty.ToString());
+            var response = await _trade.PlaceOrder(Category.SPOT, symbol, side, orderType, qty.ToString());
             if (!string.IsNullOrEmpty(response))
             {
                 var responseModel = JsonConvert.DeserializeObject<PlaceOrderResult>(response);
                 return responseModel ?? new PlaceOrderResult() { OrderId = string.Empty, OrderLinkId = string.Empty };
             }
-            
+
             return new PlaceOrderResult() { OrderId = string.Empty, OrderLinkId = string.Empty };
+        }
+
+        public async Task<WalletBalanceResponse?> GetWalletBalanceAsync(AccountType accountType)
+        {
+            return await _account.GetWalletBalanceAsync(accountType);
         }
     }
 }
