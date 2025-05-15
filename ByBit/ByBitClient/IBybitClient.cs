0adfda8d902d27612055e08782ad8c9481d1eee0
Vit <79087978+TNSGXhosts@users.noreply.github.com>
2025-05-15 13:26:39 +0200

    feat: add GetWalletBalanceAsync to IBybitClient interface

--- ByBit/ByBitClient/IBybitClient.cs
+++ ByBit/ByBitClient/IBybitClient.cs
@@ -2,11 +2,14 @@ using bybit.net.api.Models.Market;
 using bybit.net.api.Models.Trade;
 using MyProject.Domain.BybitModels.Prices;
 using MyProject.Domain.BybitModels.Trading;
+using MyProject.Domain.BybitModels.Account;
 
-namespace Bybit.BybitClient;
-
-public interface IBybitClient
+namespace Bybit.BybitClient
 {
-    Task<List<Kline>> GetKlinesAsync(string symbol, MarketInterval interval, int? limit = null);
-    Task<PlaceOrderResult> PlaceOrderAsync(string symbol, Side side, OrderType orderType, decimal qty);
+    public interface IBybitClient
+    {
+        Task<List<Kline>> GetKlinesAsync(string symbol, MarketInterval interval, int? limit = null);
+        Task<PlaceOrderResult> PlaceOrderAsync(string symbol, Side side, OrderType orderType, decimal qty);
+        Task<WalletBalance?> GetWalletBalanceAsync(string accountType);
+    }
 }
